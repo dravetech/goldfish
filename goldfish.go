cb350fa0cef1e9cf87e1aaa9c73990b85848ef8b
Gabriele <gabrielegerbino@gmail.com>
2020-10-28 09:04:41 +0100

    Add support for comparing files with list of jsos (#1)

--- goldfish.go
+++ goldfish.go
@@ -10,6 +10,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
@@ -17,15 +18,16 @@ import (
 
 // CommandTestCase case is a struct that defines a test case for a command
 type CommandTestCase struct {
-	Name        string   // Name of the test case, it will be used as filename for the golden files
-	GoldenPath  string   // Path to the golden files
-	Command     []string // Command to run
-	Update      bool     // Update the golden files for this test case
-	ExitCode    int      // Expected exit code
-	StdoutRegex bool
-	StderrRegex bool
-	StdoutJSON  bool
-	StderrJSON  bool
+	Name           string   // Name of the test case, it will be used as filename for the golden files
+	GoldenPath     string   // Path to the golden files
+	Command        []string // Command to run
+	Update         bool     // Update the golden files for this test case
+	ExitCode       int      // Expected exit code
+	StdoutRegex    bool
+	StderrRegex    bool
+	StdoutJSON     bool
+	StdoutJSONList bool
+	StderrJSON     bool
 }
 
 func (tc *CommandTestCase) StdoutGoldenPath() string {
@@ -62,8 +64,8 @@ func (tc *CommandTestCase) Run(t *testing.T) {
 		t.Errorf("exit code doesn't match; got (%d), expected (%d)\n", exitCode, tc.ExitCode)
 	}
 
-	compareGolden(t, tc.Update, tc.StdoutGoldenPath(), stdout, "stdout", tc.StdoutRegex, tc.StdoutJSON)
-	compareGolden(t, tc.Update, tc.StderrGoldenPath(), stderr, "stderr", tc.StderrRegex, tc.StderrJSON)
+	compareGolden(t, tc.Update, tc.StdoutGoldenPath(), stdout, "stdout", tc.StdoutRegex, tc.StdoutJSON, tc.StdoutJSONList)
+	compareGolden(t, tc.Update, tc.StderrGoldenPath(), stderr, "stderr", tc.StderrRegex, tc.StderrJSON, tc.StdoutJSONList)
 }
 
 func compareGoldenString(t *testing.T, update bool, path string, buf bytes.Buffer, out string, useRegex bool) {
@@ -82,12 +84,16 @@ func compareGoldenString(t *testing.T, update bool, path string, buf bytes.Buffe
 	}
 }
 
-func compareGoldenJSON(t *testing.T, update bool, path string, buf bytes.Buffer, out string, useRegex bool) {
-	goldenOut := get(t, buf.Bytes(), path, update, true)
-
+func compareJSON(t *testing.T, goldenBytes []byte, gotBytes []byte, useRegex bool, out string) {
 	goldenData := new(interface{})
-	if err := json.Unmarshal(goldenOut, goldenData); err != nil {
+	if err := json.Unmarshal(goldenBytes, goldenData); err != nil {
 		t.Log("couldn't unmarshal golden data: " + err.Error())
+		t.FailNow()
+	}
+
+	gotData := new(interface{})
+	if err := json.Unmarshal(gotBytes, gotData); err != nil {
+		t.Log("couldn't unmarshal gotten data: " + err.Error())
 		t.FailNow()
 	}
 
@@ -106,21 +112,35 @@ func compareGoldenJSON(t *testing.T, update bool, path string, buf bytes.Buffer,
 		}))
 	}
 
-	gotData := new(interface{})
-	if err := json.Unmarshal(buf.Bytes(), gotData); err != nil {
-		t.Log("couldn't unmarshal gotten data: " + err.Error())
-		t.FailNow()
-	}
-
 	if !cmp.Equal(gotData, goldenData, opts...) {
 		t.Errorf("%s doesn't match:\n%s", out, cmp.Diff(gotData, goldenData, opts...))
 	}
 
 }
 
-func compareGolden(t *testing.T, update bool, path string, buf bytes.Buffer, out string, useRegex bool, useJSON bool) {
+func compareGoldenJSON(t *testing.T, update bool, path string, buf bytes.Buffer, out string, useRegex bool) {
+	goldenOut := get(t, buf.Bytes(), path, update, true)
+	compareJSON(t, goldenOut, buf.Bytes(), useRegex, out)
+}
+
+func compareGoldenJSONList(t *testing.T, update bool, path string, buf bytes.Buffer, out string, useRegex bool) {
+	goldenOut := get(t, buf.Bytes(), path, update, false)
+
+	goldenOutLines := strings.Split(string(goldenOut), "\n")
+	goldenDataLines := strings.Split(buf.String(), "\n")
+	for i := range goldenOutLines {
+		if goldenOutLines[i] == "" {
+			continue
+		}
+		compareJSON(t, []byte(goldenOutLines[i]), []byte(goldenDataLines[i]), useRegex, out)
+	}
+}
+
+func compareGolden(t *testing.T, update bool, path string, buf bytes.Buffer, out string, useRegex bool, useJSON bool, useJSONList bool) {
 	if useJSON {
 		compareGoldenJSON(t, update, path, buf, out, useRegex)
+	} else if useJSONList {
+		compareGoldenJSONList(t, update, path, buf, out, useRegex)
 	} else {
 		compareGoldenString(t, update, path, buf, out, useRegex)
 	}
